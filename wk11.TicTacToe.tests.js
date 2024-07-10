3d1becb4fc5ff129001c09fd35bca0a2097930d3
techydude9 <bob.ruzga@gmail.com>
2024-07-10 14:14:19 -0400

    Base Code

---
+++ wk11.TicTacToe.tests.js
@@ -0,0 +1,11 @@
+/* Week 11 Front End Dev
+    Author: Bob Ruzga
+    Date: July 7, 2024
+    Testing js file
+
+    Installed an older version of Mocha and Chai
+    see package.json
+    */
+
+    const expect = chai.expect
+    const assert = chai.assert
